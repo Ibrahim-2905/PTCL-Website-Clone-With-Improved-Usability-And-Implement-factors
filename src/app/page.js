a56aa0ee63dd8dbd1a2ae5df213f5a117333abc2
pretender-2905 <pretender2905@gmail.com>
2026-05-12 12:02:59 +0500

    support section

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Hero from "./components/hero";
 import LatestOffers from "./components/latestOffers";
+import Support from "./components/support"
 
 export default function Home() {
     return (
         <div>
             <Hero />
             <LatestOffers />
+            <Support />
         </div>
     )
 }
--- src/app/utils/colors.js
+++ src/app/utils/colors.js
@@ -2,4 +2,5 @@ export const gradient = "linear-gradient(to right, rgb(139, 184, 39) 0%, rgb(0, 
 export const greenDark = "#006736";
 export const white = "#ffffff";
 export const lightGreen = "#A5CC4D";
-export const black = "#000000"
+export const black = "#000000";
+export const darkGray = "#313131";
